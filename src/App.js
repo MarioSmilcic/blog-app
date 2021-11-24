82e21ef458551f9e75cb611ba7e9be58c3e5abcb
Mario Smilcic <mariosmilcic@gmail.com>
2021-11-24 18:17:46 +0100

    add mui to project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import react from 'react';
+import Button from '@mui/material/Button';
+import React from 'react';
+
+const App = () => {
+  return (
+    <>
+      <div>hello world</div>
+      <Button variant="contained">Hello World</Button>;
+    </>
+  );
+};
+
+export default App;
