8a40a22ff10cb9c6640c3fb006fdb34f5b3905ab
Mario Smilcic <mariosmilcic@gmail.com>
2021-12-01 16:46:37 +0100

    app design

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,9 +1,26 @@
 import styles from './Hero.module.css';
+import Navbar from '../Navbar/Navbar';
+import OurButton from '../OurButton/OurButton';
+import { Typography } from '@mui/material';
 
 const Hero = () => {
   return (
-    <div className={styles.hero}>
-      <h1>Hello, i'm Hero</h1>
+    <div>
+      <div className={styles.hero}>
+        <Typography
+          color="primary"
+          variant="h4"
+          className={styles.text}
+          color="#fff"
+        >
+          "Ovo je nas blog. Nas blog je ovo. Ovo je nas blog" (Mika Antic)
+        </Typography>
+        <div>
+          <OurButton />
+        </div>
+      </div>
+
+      <Navbar />
     </div>
   );
 };
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,12 +1,69 @@
 import styles from './Navbar.module.css';
-import { style } from '@mui/system';
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  List,
+  ListItem,
+  ListItemText,
+  Link,
+} from '@mui/material';
 
 const Navbar = () => {
   return (
-    <div className={styles.Navbar}>
-      <h1>hello from navbar</h1>
+    <div>
+      <AppBar color="transparent" className={styles.appbar} elevation={0}>
+        <Toolbar>
+          <Typography variant="h4" color="white" className={styles.pictures}>
+            pictures
+          </Typography>
+          <List className={styles.list}>
+            <ListItem>
+              <Link underline="hover" color="white">
+                videos
+              </Link>
+            </ListItem>
+            <ListItem>
+              <Link underline="hover" color="white">
+                about
+              </Link>
+            </ListItem>
+            <ListItem>
+              <Link underline="hover" color="white">
+                contact
+              </Link>
+            </ListItem>
+          </List>
+        </Toolbar>
+      </AppBar>
     </div>
   );
 };
 
 export default Navbar;
+
+/* const Navbar = () => {
+  return (
+    <div>
+      <AppBar className={styles.navbar} elevation={0}>
+        <Toolbar>
+          <h1>Pictures</h1>
+          <ul>
+            <li>
+              <a href="/#">videos</a>
+            </li>
+            <li>
+              <a href="/#">about</a>
+            </li>
+            <li>
+              <a href="/#">contact</a>
+            </li>
+          </ul>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+};
+
+export default Navbar;
+ */
