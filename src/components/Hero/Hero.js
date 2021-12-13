ac8f1390f89ce7a77263fcea1dd7ef12cae49e9f
Mario Smilcic <mariosmilcic@gmail.com>
2021-12-13 11:20:15 +0100

    deleting comments

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -12,12 +12,6 @@ const Hero = () => {
             sm={2}
           ></Row>
 
-          {/* <Row>
-            <h2>
-              "Ovo je naš blog. Naš blog je ovo. Ovo je naš blog" (Mika Antić)
-            </h2>
-          </Row> */}
-
           <Row className="justify-content-md-center text-center mt-5 pt-5 gy-5">
             <OurButton />
           </Row>
